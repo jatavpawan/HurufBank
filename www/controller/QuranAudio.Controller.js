ed61ef817efb761ce3ac4713bdc0f8756713f354
AJAY\shiva <shivajay@hotmail.com>
2017-08-30 10:44:33 +0530

    submitted changes

--- www/controller/QuranAudio.Controller.js
+++ www/controller/QuranAudio.Controller.js
@@ -1,6 +1,8 @@
-﻿angular.module('Quran.Audio.module', []).controller('Quran.Audio.Controller', function ($scope, $rootScope, $state, $ionicPlatform, $cordovaFileTransfer,httpServices) {
+﻿angular.module('Quran.Audio.module', []).controller('Quran.Audio.Controller', function ($scope, $rootScope, $state, $ionicPlatform, $cordovaFileTransfer, httpServices, $timeout, $cordovaMedia) {
+  var media = '';
 
   $scope.Files = {};
+  $scope.pause=true;
   var queryStr = 'FileType=3&FileCategory=' + $rootScope.footerIcoSelection;
   httpServices.get('/GetFiles?' + queryStr).then(function (response) {
     $scope.Files = response.data;
@@ -37,51 +39,33 @@
   }
 
   $scope.playTrack = function (track, key) {
-    var filename = track.split("/").pop();
+    if (media != '') {
+      var filename = track.split("/").pop();
 
-    var targetPath = cordova.file.externalRootDirectory + "Huruf/Audio/" + filename;
-    alert(targetPath);
-    // Preload an audio track before we play it
-    // window.plugins.NativeAudio.preloadComplex(key, targetPath, 1, 1, 0, function(msg) {
-    //   // If this is not a first playback stop and unload previous audio track
-    //   if ($scope.player.key.length > 0) {
-    //     window.plugins.NativeAudio.stop($scope.player.key); // Stop audio track
-    //     window.plugins.NativeAudio.unload($scope.player.key); // Unload audio track
-    //   }
+      var targetPath = cordova.file.externalRootDirectory + "Huruf/Audio/1.mp3";// + filename;
+      alert(targetPath);
+      media = $cordovaMedia.newMedia(targetPath);
+      // var iOSPlayOptions = {
+      //   numberOfLoops: 2,
+      //   playAudioWhenScreenIsLocked: false
+      // }
 
-    //   window.plugins.NativeAudio.play(key); // Play audio track
-    //   $scope.player.key = key; // Set a current audio track so we can close it if needed 
-    // }, function(msg) {
-    //   console.log('error: ' + msg); // Loading error
-    // });
-    // var audioUrl = track;
-
-    // // Play an audio file (not recommended, since the screen will be plain black)
-    // window.plugins.streamingMedia.playAudio(track);
-
-    // Play an audio file with options (all options optional)
-    var options = {
-      bgColor: "#FFFFFF",
-      //bgImage: "<SWEET_BACKGROUND_IMAGE>",
-      bgImageScale: "fit", // other valid values: "stretch"
-      initFullscreen: false, // true(default)/false iOS only
-      successCallback: function () {
-        console.log("Player closed without error.");
-      },
-      errorCallback: function (errMsg) {
-        console.log("Error! " + errMsg);
-      }
-    };
-    window.plugins.streamingMedia.playAudio(targetPath, options);
-  };
-
-  $scope.stopTrack = function () {
-    // If this is not a first playback stop and unload previous audio track
-    if ($scope.player.key.length > 0) {
-      window.plugins.NativeAudio.stop($scope.player.key); // Stop audio track
-      window.plugins.NativeAudio.unload($scope.player.key); // Unload audio track
-      $scope.player.key = ''; // Remove a current track on unload, it will break an app if we try to unload it again in playTrack function
+      //media.play(iOSPlayOptions); // iOS only!
+      media.play(); // Android
+      console.log(media.getDuration());
     }
+    else {
+      media.play();
+    }
+    $scope.pause=false;
+  };
+  $scope.pauseTrack = function () {
+    media.pause();
+    $scope.pause=true;
+  };
+  $scope.stopTrack = function () {
+    media.stop();
+    media.release();
   };
   $scope.AddFile = function () {
     $rootScope.TabID = "3";
--- www/controller/viewfile.controller.js
+++ www/controller/viewfile.controller.js
@@ -1,5 +1,6 @@
 angular.module('viewfile.module.controller', []).controller('viewfile.controller', function ($scope, $rootScope, $ionicHistory, pdfDelegate, $stateParams) {
-    $scope.relativity =  $stateParams.fileURL;//'http://gmcsco.com/para.pdf';
+    $rootScope.loginStatus = false;
+    $scope.relativity = $stateParams.fileURL;//'http://gmcsco.com/para.pdf';
     $scope.material = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/149125/material-design-2.pdf';
     $scope.pdfUrl = $scope.relativity;
     $scope.goBack = function () {
